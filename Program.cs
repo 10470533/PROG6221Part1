2af4a35c8998936c684d0e4ac97edf8a893ae32c
ST10470533 <masegomm.malatji@gmail.com>
2026-03-30 22:04:35 +0200

    Added different objects of every class and calling methods to main class

--- Program.cs
+++ Program.cs
@@ -5,15 +5,24 @@
         static void Main(string[] args)
         {
 
-            ASCII hnd= new ASCII();
-            hnd.Heading();
+            // Create an object of the ASCII class
+            ASCII prt = new ASCII();
 
-            Audio.PlayGreet(); 
+            // Call the Heading() method to print the chatbot title/design
+            prt.Heading();
 
-            
+            // Create an object of the Sound class (optional here since method is static)
+            Audio snd = new Audio();
+
+            // Call the PlayGreet() method to play a welcome audio sound
+            Audio.PlayGreet();
 
 
+            // Create an object of the BasicResponses class
+            Response br = new Response();
 
+            // Call the Response() method to start the chatbot conversation loop
+            br.BasicResponse();
         }
     }
 }
