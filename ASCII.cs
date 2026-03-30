63649fc272a01b8a914ac8f4cb25824ee6a68e0b
ST10470533 <masegomm.malatji@gmail.com>
2026-03-30 18:16:58 +0200

    Added a styled heading using ASCII art

--- ASCII.cs
+++ ASCII.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Figgle.Fonts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,15 @@ namespace ST10470533_POE1
 {
     internal class ASCII
     {
+        // Displays a styled heading in the console using ASCII art
+           public void Heading()
+        {
+            string msg = "Cybersecurity Awareness Bot";
+            Console.ForegroundColor= ConsoleColor.Blue;
+            string ascii = FiggleFonts.Small.Render(msg);
+
+            Console.WriteLine(ascii);
+            Console.ResetColor();
+        }
     }
 }
