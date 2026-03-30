c9b9bdb1d5db91285fdde3768b58a78e83c66612
ST10470533 <masegomm.malatji@gmail.com>
2026-03-30 10:56:06 +0200

    initailize my project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ST10470533_POE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voice snd= new Voice();
+            snd.Pl
+        }
+    }
+}
