a6c5f6945922f715d3fbbb5dd7bc25816f7d012b
ST10470533 <masegomm.malatji@gmail.com>
2026-03-30 20:40:26 +0200

    Added a method that collects user details in Response class

--- Response .cs
+++ Response .cs
@@ -6,5 +6,23 @@ namespace ST10470533_POE1
 {
     internal class Response
     {
+        // This method collects user details (name) and returns it
+        public string Details()
+        {
+            string name;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*      USER DETAILS                    *");
+            Console.WriteLine("****************************************");
+            Console.ResetColor();
+
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine($"Welcome, {name}!");
+
+            return name;
+
+        }
     }
 }
